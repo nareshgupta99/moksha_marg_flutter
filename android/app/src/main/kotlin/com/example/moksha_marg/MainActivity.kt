244760c3cd4369db958a7b51364b5d389114cdb3
Naresh Gupta <nareshgupta0899@gmail.com>
2025-03-16 19:34:07 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/moksha_marg/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.moksha_marg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MokshaMarg());
+}
+
+class MokshaMarg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      
+    );
+  }
+}
