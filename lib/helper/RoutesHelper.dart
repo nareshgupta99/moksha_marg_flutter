899ed3f804e3cb04096008e7efb8f0d5df772de7
Naresh Gupta <nareshgupta0899@gmail.com>
2025-03-16 19:53:30 +0530

    routes setup

---
+++ lib/helper/RoutesHelper.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'package:moksha_marg/component/authentication/registration.dart/view/registration.dart';
+import 'package:moksha_marg/component/home/view/home.dart';
+
+class RoutesHelper {
+  static const String _home = "/home";
+  static const String _registration = "/registration ";
+
+  static String getHome() => _home;
+  static String getRegistration() => _registration;
+
+  static List<GetPage> routes = [
+    GetPage(name: _home, page: () => Home()),
+    GetPage(name: _registration, page: () => Registration())
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:moksha_marg/component/home/view/home.dart';
+import 'package:moksha_marg/helper/RoutesHelper.dart';
 
 void main() {
   runApp(MokshaMarg());
@@ -9,7 +11,9 @@ class MokshaMarg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      
+      initialRoute: RoutesHelper.getHome(),
+      getPages: RoutesHelper.routes,
+      home: Home(),
     );
   }
 }
